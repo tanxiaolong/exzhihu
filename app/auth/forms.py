42527202a142be2f57de954fc218e8ed2c643541
oddcc <skycc716@gmail.com>
2016-11-14 22:09:29 +0800

    无bug 修改密码 重置密码 修改邮箱

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -28,3 +28,33 @@ class RegistrationForm(Form):
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError(u'用户名已被注册,请换一个试试')
+
+
+class ChangePasswordForm(Form):
+    old_password = PasswordField(u'当前密码', validators=[Required()])
+    password = PasswordField(u'新密码', validators=[
+        Required(), EqualTo('password2', message=u'密码必须相同')])
+    password2 = PasswordField(u'确认新密码', validators=[Required()])
+    submit = SubmitField(u'更新密码')
+
+
+class ResetPasswordRequestForm(Form):
+    email = StringField(u'注册邮箱', validators=[Required(), Email()])
+    submit = SubmitField(u'找回密码')
+
+
+class ResetPasswordForm(Form):
+    email = StringField(u'注册邮箱', validators=[Required(), Email()])
+    password = PasswordField(u'新密码', validators=[Required(), EqualTo('password2', message=u'两次密码不相同')])
+    password2 = PasswordField(u'确认新密码', validators=[Required()])
+    submit = SubmitField(u'修改密码')
+
+
+class ChangeEmailForm(Form):
+    email = StringField(u'新邮箱', validators=[Required(), Email()])
+    password = PasswordField(u'密码', validators=[Required()])
+    submit = SubmitField(u'修改邮箱')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(u'邮箱已被注册,请换一个试试')
